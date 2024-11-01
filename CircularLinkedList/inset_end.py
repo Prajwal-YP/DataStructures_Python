f349ad90bbe07bdbb2cea2ab574dbea23930e2a9
Prajwal_Y_P <87578946+Prajwal-YP@users.noreply.github.com>
2024-11-01 22:34:41 +0530

    Create inset_end.py

---
+++ CircularLinkedList/inset_end.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,value:int):
+        self.value=value
+        self.next=None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.headNode:Node=None
+        self.tailNode:Node=None
+
+    def inset_end(self,value:int):
+
+        new_node:Node=Node(value)
+        
+        if self.headNode is None:
+            self.headNode=new_node
+            self.headNode.next=self.headNode
+            self.tailNode=self.headNode
+            return 
+        
+        new_node.next=self.headNode
+        self.headNode=new_node
+        self.tailNode.next=new_node
+
+    def print_values(self):
+        current_node=self.headNode
+        print('Values of circular linked list is :\t',end='')
+        if current_node is None:
+            return
+        print(current_node.value,end=',')
+        current_node=current_node.next
+        while current_node!=self.headNode:
+            print(current_node.value,end=',')
+            current_node=current_node.next
+        print('\b')
+
+
+list1=CircularLinkedList()
+
+list1.inset_end(400)
+list1.inset_end(300)
+list1.inset_end(200)
+list1.inset_end(100)
+list1.print_values()
